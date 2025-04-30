e9ee5f25d9868c072abdafcabd3c73e9ddd017f2
Vaibhav <vaibhavsinghvrajpot@gmail.com>
2025-05-01 03:31:07 +0530

    removed eslint warning

--- app/api/get-country/route.ts
+++ app/api/get-country/route.ts
@@ -4,7 +4,7 @@ import { getCountryFromCoords } from '@/lib/geo';
 export async function GET(req: NextRequest) {
   const lat = req.nextUrl.searchParams.get('lat');
   const lng = req.nextUrl.searchParams.get('lng');
-  console.log('lat----------lng', lat , lng);
+ 
   if (!lat || !lng) {
     return NextResponse.json({ error: 'Missing coordinates' }, { status: 400 });
   }
@@ -13,6 +13,7 @@ export async function GET(req: NextRequest) {
     const countryCode = await getCountryFromCoords(lat, lng);
     return NextResponse.json({ countryCode });
   } catch (err) {
+     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     return NextResponse.json({ error: 'Failed to get country' }, { status: 500 });
   }
 }
